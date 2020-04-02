e7927dbff4d83d43f1ed3369b1d14d0b746b3cb3
Ryan Pavlik <ryan.pavlik@gmail.com>
2020-04-02 11:36:21 -0500

    First pages commit, manual

---
+++ today.js
@@ -0,0 +1,39 @@
+!function(){"use strict";
+// Copyright 2020, Ryan Pavlik <ryan.pavlik@gmail.com>
+// SPDX-License-Identifier: MIT
+/** Get a Date for today.
+ * @return {Date}
+*/
+/** Get the AMP article URL.
+ * @param {Date} d Date for article URL.
+ *
+ * @return {String} url
+ */
+// Object.assign(exports, {
+//   today,
+//   tomorrow,
+//   yesterday,
+//   getArticle,
+//   getAMPUrl,
+//   getMainUrl,
+// });
+// Copyright 2020, Ryan Pavlik <ryan.pavlik@gmail.com>
+// SPDX-License-Identifier: MIT
+window.location="https://www.cnn.com/world/live-news/"+
+/** Get a Date for tomorrow.
+ * @return {Date}
+ */
+/** Get a Date for yesterday.
+ * @return {Date}
+ */
+/** Get the last component of the article URL.
+ * @param {Date} d Date for article URL.
+ *
+ * @return {String} last component of the article URL
+ */
+function(t){return"coronavirus-pandemic-"+String(t.getMonth()+1).padStart(2,"0")+"-"+String(t.getDate()).padStart(2,"0")+"-20-intl/"}
+/** Get the desktop article URL.
+ * @param {Date} d Date for article URL.
+ *
+ * @return {String} url, like https://www.cnn.com/world/live-news/coronavirus-pandemic-04-02-20-intl/
+ */(new Date(Date.now()))}();
---
+++ yesterday.js
@@ -0,0 +1,40 @@
+!function(){"use strict";
+// Copyright 2020, Ryan Pavlik <ryan.pavlik@gmail.com>
+// SPDX-License-Identifier: MIT
+/** Get a Date for today.
+ * @return {Date}
+*/
+/** Get a Date for tomorrow.
+ * @return {Date}
+ */
+/** Get a Date for yesterday.
+ * @return {Date}
+ */
+/** Get the AMP article URL.
+ * @param {Date} d Date for article URL.
+ *
+ * @return {String} url
+ */
+// Object.assign(exports, {
+//   today,
+//   tomorrow,
+//   yesterday,
+//   getArticle,
+//   getAMPUrl,
+//   getMainUrl,
+// });
+// Copyright 2020, Ryan Pavlik <ryan.pavlik@gmail.com>
+// SPDX-License-Identifier: MIT
+window.location=
+/** Get the desktop article URL.
+ * @param {Date} d Date for article URL.
+ *
+ * @return {String} url, like https://www.cnn.com/world/live-news/coronavirus-pandemic-04-02-20-intl/
+ */
+function(t){return"https://www.cnn.com/world/live-news/"+
+/** Get the last component of the article URL.
+ * @param {Date} d Date for article URL.
+ *
+ * @return {String} last component of the article URL
+ */
+function(t){return"coronavirus-pandemic-"+String(t.getMonth()+1).padStart(2,"0")+"-"+String(t.getDate()).padStart(2,"0")+"-20-intl/"}(t)}(function(){const t=new Date(Date.now());return t.setDate(t.getDate()-1),t}())}();
