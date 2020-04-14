f5ba3ad07fdc4fa3b7c69077f6b13109219c2d69
rpavlik <rpavlik@users.noreply.github.com>
2020-04-14 20:09:37 +0000

    deploy: 7b9041a678a1c3400b1c7349c10df2e1e6871151

--- today_amp.js
+++ today_amp.js
@@ -1,39 +1,20 @@
 !function(){"use strict";
 // Copyright 2020, Ryan Pavlik <ryan.pavlik@gmail.com>
 // SPDX-License-Identifier: MIT
-/** Get a Date for today.
- * @return {Date}
-*/
-// Object.assign(exports, {
-//   today,
-//   tomorrow,
-//   yesterday,
-//   getArticle,
-//   getAMPUrl,
-//   getMainUrl,
-// });
-// Copyright 2020, Ryan Pavlik <ryan.pavlik@gmail.com>
-// SPDX-License-Identifier: MIT
-window.location="https://amp.cnn.com/cnn/world/live-news/"+
-/** Get a Date for tomorrow.
- * @return {Date}
- */
-/** Get a Date for yesterday.
- * @return {Date}
- */
 /** Get the last component of the article URL.
+ *
+ * This is the function that needs to be updated if the links start giving an
+ * error 404. It appears as though the URL creation is at least somewhat manual
+ * on CNN's side: they usually follow the pattern for at least a few days, but
+ * then may modify it slightly.
+ *
  * @param {Date} d Date for article URL.
  *
  * @return {String} last component of the article URL
  */
-function(t){return"coronavirus-pandemic-intl-"+String(t.getMonth()+1).padStart(2,"0")+"-"+String(t.getDate()).padStart(2,"0")+"-20/"}
-/** Get the desktop article URL.
- * @param {Date} d Date for article URL.
- *
- * @return {String} url, like https://www.cnn.com/world/live-news/coronavirus-pandemic-04-02-20-intl/
- */
-/** Get the AMP article URL.
- * @param {Date} d Date for article URL.
- *
- * @return {String} url
- */(new Date(Date.now()))}();
+// Copyright 2020, Ryan Pavlik <ryan.pavlik@gmail.com>
+// SPDX-License-Identifier: MIT
+window.location="https://amp.cnn.com/cnn/world/live-news/"+function(n){return["coronavirus-pandemic-intl",String(n.getMonth()+1).padStart(2,"0"),String(n.getDate()).padStart(2,"0"),"20"].join("-")}
+/** Get a Date for today.
+ * @return {Date}
+*/(new Date(Date.now()))+"/"}();
--- yesterday_amp.js
+++ yesterday_amp.js
@@ -1,6 +1,39 @@
 !function(){"use strict";
 // Copyright 2020, Ryan Pavlik <ryan.pavlik@gmail.com>
 // SPDX-License-Identifier: MIT
+/** Get the last component of the article URL.
+ *
+ * This is the function that needs to be updated if the links start giving an
+ * error 404. It appears as though the URL creation is at least somewhat manual
+ * on CNN's side: they usually follow the pattern for at least a few days, but
+ * then may modify it slightly.
+ *
+ * @param {Date} d Date for article URL.
+ *
+ * @return {String} last component of the article URL
+ */
+// Copyright 2020, Ryan Pavlik <ryan.pavlik@gmail.com>
+// SPDX-License-Identifier: MIT
+window.location=
+/** Get the desktop article URL.
+ *
+ * Contains a call to getArticle() which does the complicated (varied)
+ * part of the job. This part of the URL template seems fixed.
+ *
+ * @param {Date} d Date for article URL.
+ *
+ * @return {String} url, like https://www.cnn.com/world/live-news/coronavirus-pandemic-04-02-20-intl/
+ */
+/** Get the AMP article URL.
+ *
+ * Contains a call to getArticle() which does the complicated (varied)
+ * part of the job. This part of the URL template seems fixed.
+ *
+ * @param {Date} d Date for article URL.
+ *
+ * @return {String} url
+ */
+function(t){return"https://amp.cnn.com/cnn/world/live-news/"+function(t){return["coronavirus-pandemic-intl",String(t.getMonth()+1).padStart(2,"0"),String(t.getDate()).padStart(2,"0"),"20"].join("-")}
 /** Get a Date for today.
  * @return {Date}
 */
@@ -9,32 +42,4 @@
  */
 /** Get a Date for yesterday.
  * @return {Date}
- */
-// Object.assign(exports, {
-//   today,
-//   tomorrow,
-//   yesterday,
-//   getArticle,
-//   getAMPUrl,
-//   getMainUrl,
-// });
-// Copyright 2020, Ryan Pavlik <ryan.pavlik@gmail.com>
-// SPDX-License-Identifier: MIT
-window.location=
-/** Get the desktop article URL.
- * @param {Date} d Date for article URL.
- *
- * @return {String} url, like https://www.cnn.com/world/live-news/coronavirus-pandemic-04-02-20-intl/
- */
-/** Get the AMP article URL.
- * @param {Date} d Date for article URL.
- *
- * @return {String} url
- */
-function(t){return"https://amp.cnn.com/cnn/world/live-news/"+
-/** Get the last component of the article URL.
- * @param {Date} d Date for article URL.
- *
- * @return {String} last component of the article URL
- */
-function(t){return"coronavirus-pandemic-intl-"+String(t.getMonth()+1).padStart(2,"0")+"-"+String(t.getDate()).padStart(2,"0")+"-20/"}(t)}(function(){const t=new Date(Date.now());return t.setDate(t.getDate()-1),t}())}();
+ */(t)+"/"}(function(){const t=new Date(Date.now());return t.setDate(t.getDate()-1),t}())}();
