1d3e2b05dad4926cd1349599a1f249c2708ac5e2
Ryan Pavlik <ryan.pavlik@gmail.com>
2020-04-14 15:27:10 -0500

    Hopefully fix tests.

--- test/test.js
+++ test/test.js
@@ -3,23 +3,23 @@ const cnn = require('../app/common/shared');
 
 const bhttp = require('bhttp');
 const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 describe('covid-cnn-bookmarklet', function() {
   describe('getMainUrl', function() {
-    it('should get HTTP status 200', function() {
-      const url = cnn.getMainUrl(cnn.today());
-      return bhttp.head(url).then(function(resp) {
-        expect(resp.statusCode).to.equal(200);
-      });
+    const url = cnn.getMainUrl(cnn.today()).replace('https', 'http');
+    it('should get HTTP status 200: ' + url, function() {
+      return expect(bhttp.head(url))
+          .to.eventually.have.property('statusCode', 200);
     });
   });
   describe('getAMPUrl', function() {
-    it('should get HTTP status 200', function() {
-      const url = cnn.getAMPUrl(cnn.today());
-      return bhttp.head(url).then(function(resp) {
-        expect(resp.statusCode).to.equal(200);
-      });
+    const url = cnn.getAMPUrl(cnn.today()).replace('https', 'http');
+    it('should get HTTP status 200: ' + url, function() {
+      return expect(bhttp.head(url))
+          .to.eventually.have.property('statusCode', 200);
     });
   });
 });
