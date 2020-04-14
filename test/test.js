7b9041a678a1c3400b1c7349c10df2e1e6871151
Ryan Pavlik <ryan.pavlik@gmail.com>
2020-04-14 15:05:00 -0500

    Simplify test

--- test/test.js
+++ test/test.js
@@ -1,25 +1,25 @@
 /* eslint-disable require-jsdoc */
-// const assert = require('assert');
-
 const cnn = require('../app/common/shared');
 
 const bhttp = require('bhttp');
 const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
-const should = chai.should();
+const expect = chai.expect;
 
 describe('covid-cnn-bookmarklet', function() {
   describe('getMainUrl', function() {
     it('should get HTTP status 200', function() {
       const url = cnn.getMainUrl(cnn.today());
-      return bhttp.head(url).should.eventually.have.property('statusCode').that.equals(200);
+      return bhttp.head(url).then(function(resp) {
+        expect(resp.statusCode).to.equal(200);
+      });
     });
   });
   describe('getAMPUrl', function() {
     it('should get HTTP status 200', function() {
       const url = cnn.getAMPUrl(cnn.today());
-      return bhttp.head(url).should.eventually.have.property('statusCode').that.equals(200);
+      return bhttp.head(url).then(function(resp) {
+        expect(resp.statusCode).to.equal(200);
+      });
     });
   });
 });
