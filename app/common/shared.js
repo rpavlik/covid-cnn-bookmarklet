ef88395d6666a28ce548b4c24327623fae2ef115
Ryan Pavlik <ryan.pavlik@gmail.com>
2020-04-14 14:24:40 -0500

    minor cleanups for maintainability

--- app/common/shared.js
+++ app/common/shared.js
@@ -1,6 +1,26 @@
 // Copyright 2020, Ryan Pavlik <ryan.pavlik@gmail.com>
 // SPDX-License-Identifier: MIT
 
+/** Get the last component of the article URL.
+ *
+ * This is the function that needs to be updated if the links start giving an
+ * error 404. It appears as though the URL creation is at least somewhat manual
+ * on CNN's side: they usually follow the pattern for at least a few days, but
+ * then may modify it slightly.
+ *
+ * @param {Date} d Date for article URL.
+ *
+ * @return {String} last component of the article URL
+ */
+export function getArticle(d) {
+  return [
+    'coronavirus-pandemic-intl',
+    String(d.getMonth() + 1).padStart(2, '0'),
+    String(d.getDate()).padStart(2, '0'),
+    '20',
+  ].join('-');
+}
+
 /** Get a Date for today.
  * @return {Date}
 */
@@ -24,43 +44,29 @@ export function yesterday() {
   return d;
 }
 
-/** Get the last component of the article URL.
- * @param {Date} d Date for article URL.
- *
- * @return {String} last component of the article URL
- */
-export function getArticle(d) {
-  return [
-    'coronavirus-pandemic-intl',
-    String(d.getMonth() + 1).padStart(2, '0'),
-    String(d.getDate()).padStart(2, '0'),
-    '20/',
-  ].join('-');
-}
 
 /** Get the desktop article URL.
+ *
+ * Contains a call to getArticle() which does the complicated (varied)
+ * part of the job. This part of the URL template seems fixed.
+ *
  * @param {Date} d Date for article URL.
  *
  * @return {String} url, like https://www.cnn.com/world/live-news/coronavirus-pandemic-04-02-20-intl/
  */
 export function getMainUrl(d) {
-  return 'https://www.cnn.com/world/live-news/' + getArticle(d);
+  return 'https://www.cnn.com/world/live-news/' + getArticle(d) + '/';
 }
 
 /** Get the AMP article URL.
+ *
+ * Contains a call to getArticle() which does the complicated (varied)
+ * part of the job. This part of the URL template seems fixed.
+ *
  * @param {Date} d Date for article URL.
  *
  * @return {String} url
  */
 export function getAMPUrl(d) {
-  return 'https://amp.cnn.com/cnn/world/live-news/' + getArticle(d);
+  return 'https://amp.cnn.com/cnn/world/live-news/' + getArticle(d) + '/';
 }
-
-// Object.assign(exports, {
-//   today,
-//   tomorrow,
-//   yesterday,
-//   getArticle,
-//   getAMPUrl,
-//   getMainUrl,
-// });
