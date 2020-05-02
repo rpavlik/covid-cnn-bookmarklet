8144760fdfac2127e2b5b43631c5b86574200bb4
Ryan A. Pavlik <ryan.pavlik@collabora.com>
2020-05-02 09:18:56 -0500

    Update shared.js
    fix URL

--- app/common/shared.js
+++ app/common/shared.js
@@ -14,10 +14,10 @@
  */
 export function getArticle(d) {
   return [
-    'coronavirus-pandemic-intl',
+    'coronavirus-pandemic,
     String(d.getMonth() + 1).padStart(2, '0'),
     String(d.getDate()).padStart(2, '0'),
-    '20',
+    '20-intl',
   ].join('-');
 }
 
